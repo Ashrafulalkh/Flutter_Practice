26897deddf1cce89515c40869a585aec4a6fb43e
ashra <ashrafulkhan194@gmail.com>
2024-04-04 22:54:02 +0600

    Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,127 +1,112 @@
-import 'package:app1/Home.dart';
-import 'package:app1/Login.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(Main()); //Application
+  runApp(MainApp());
 }
 
-class Main extends StatelessWidget {
+class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Signup(),
-      theme: ThemeData(
-        inputDecorationTheme: InputDecorationTheme(
-
-        ),
-      ),
+      home: Home(),
     );
   }
+
 }
 
-  class Signup extends StatelessWidget {
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeScreen();
+  }
+
+}
+
+class _HomeScreen extends State<Home> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print('init');
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    print('dependencies');
+  }
+
+  @override
+  void didUpdateWidget(covariant Home oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+    print('update widget');
+  }
+
+  int count = 0;
+
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-  backgroundColor: Colors.white,
-  appBar: AppBar(
-    backgroundColor: Colors.red,
-    title: Text('Signup Page',style: TextStyle(color: Colors.white),),
-  ),
-  body: Column(
-  children: [
-  Padding(
-    padding: const EdgeInsets.fromLTRB(50, 50, 50, 10),
-    child: TextField(
-      style: TextStyle(fontSize: 15,fontWeight: FontWeight.w600),
-      decoration: InputDecoration(
-        hintText: 'Enter Your Email',
-        hintStyle: TextStyle(fontSize: 15,fontWeight: FontWeight.w400),
-        labelText: 'Email',
-        labelStyle: TextStyle(fontSize: 12,fontWeight: FontWeight.w600),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(18),
-          borderSide: BorderSide(width: 2,color: Colors.grey),
-        )
+    return Scaffold(
+      backgroundColor:Colors.white,
+      appBar: AppBar(
+        title: const Text('Home'),
+        backgroundColor: Colors.green.shade300,
+        leading: const Icon(Icons.home_filled,color: Colors.red,),
+      ),
+      body: Center(
+        child: Text(count.toString(),style: const TextStyle(fontSize: 40),),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              count --;
+              print(count);
+              setState(() {});
+            },
+            child: const Icon(Icons.remove) ,),
+          SizedBox(width: 15,),
+          FloatingActionButton(
+            onPressed: () {
+              count ++;
+              print(count);
+              setState(() {});
+            },
+            child: const Icon(Icons.add) ,),
+          SizedBox(width: 15,),
+          FloatingActionButton(
+            onPressed: () {
+              count = 0;
+              print(count);
+              setState(() {});
+            },
+            child: const Icon(Icons.clear) ,),
+        ],
       ),
 
-    ),
-  ),
-    Padding(
-      padding: const EdgeInsets.fromLTRB(50, 5, 50, 10),
-      child: TextField(
-        style: TextStyle(fontSize: 15,fontWeight: FontWeight.w600),
-        decoration: InputDecoration(
-            hintText: 'Enter Your Password',
-            hintStyle: TextStyle(fontSize: 15,fontWeight: FontWeight.w400),
-            labelText: 'Password',
-            labelStyle: TextStyle(fontSize: 12,fontWeight: FontWeight.w600),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(18),
-              borderSide: BorderSide(width: 2,color: Colors.grey),
-            )
-        ),
-
-      ),
-    ),
-
-    Padding(
-      padding: const EdgeInsets.fromLTRB(50, 5, 50, 10),
-      child: TextField(
-        style: TextStyle(fontSize: 15,fontWeight: FontWeight.w600),
-        decoration: InputDecoration(
-            hintText: 'Enter Your Password Again',
-            hintStyle: TextStyle(fontSize: 15,fontWeight: FontWeight.w400),
-            labelText: 'Confirm Password',
-            labelStyle: TextStyle(fontSize: 12,fontWeight: FontWeight.w600),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(18),
-              borderSide: BorderSide(width: 2,color: Colors.grey),
-            )
-        ),
-
-      ),
-    ),
-    Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        Padding(
-          padding: const EdgeInsets.fromLTRB(0, 10, 20, 0),
-          child: ElevatedButton(style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.blueAccent.shade400,
-          ),
-              onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return LogIn();
-              },),);
-              },
-              child: Text('Log In',style: TextStyle(fontSize: 15,color: Colors.black),)
-          ),
-        ),
-
-        Padding(
-          padding: const EdgeInsets.fromLTRB(0, 10, 50, 0),
-          child: ElevatedButton(style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.green.shade400,
-          ),
-              onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) {
-              return Home();
-            },),);
-              },
-              child: Text('Sign Up',style: TextStyle(fontSize:15,color: Colors.white),)
-          ),
-        ),
-      ],
-    ),
-  ],
-  ),
-
-  );
+    );
   }
 
+  @override
+  void deactivate() {
+    // TODO: implement deactivate
+    super.deactivate();
+    print('deactivate');
   }
 
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    print('dispose');
+  }
+
+}
 
 
